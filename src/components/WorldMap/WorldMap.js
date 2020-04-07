c49ba7c0f7fc60f3ffb475f4ed2b4e48653184ee
gilp <gil.palikaras@gmail.com>
2020-04-07 17:55:49 +0300

    use axios use async await to load data from api render svg map with country stats and hover tooltip

--- src/components/WorldMap/WorldMap.js
+++ src/components/WorldMap/WorldMap.js
@@ -1,14 +1,85 @@
 import React,{Component} from "react";
+import './WorldMap.css';
 import WorldMapSvg from "./WorldMapSvg";
+
+const color1 = '#ffe5e5';
+const color2 = '#ff8080';
+const color3 = '#ff1a1a';
+const color4 = '#b30000';
+const color5 = '#4d0000';
 
 class WorldMap extends Component{
 
-    componentDidMount() {
-        console.log("in world map: "+this.props.country_stats);
-    }
+    /**
+     *
+     * @param countryName
+     * @param countryStats
+     */
+    renderCountryInMap = (countryName, countryStats) => {
+        const svgCountry = document.querySelector('[title="'+countryName+'"]');
+        //fill country with color depending on value of total confirmed cases
+        if(svgCountry && countryStats && countryStats.hasOwnProperty('confirmed')){
+            const svgStatShow = countryStats.confirmed;
+            svgCountry.style.fill = this.fillCountryColor(svgStatShow);
+            svgCountry.setAttribute("datainfo","<p>"+countryName+"</p><p>Confirmed:"+countryStats.confirmed+"</p><p>Deaths:"+countryStats.deaths+"<p><p>Recovered:"+countryStats.recovered+"<p>");
+        }else{
+            svgCountry.setAttribute("datainfo","<p>"+countryName+"</p><p>No data</p>");
+            console.log("could not fill country "+countryName);
+        }
+    };
+
+    /**
+     *
+     * @param countryTotal
+     * @returns {string}
+     */
+    fillCountryColor = (countryTotal) => {
+        if(countryTotal <= 999){return color1;} else
+        if(countryTotal >= 1000 && countryTotal <= 9999){return color2;} else
+        if(countryTotal >= 10000 && countryTotal <= 99999){return color3;} else
+        if(countryTotal >= 100000){return color4;}
+    };
+
+    /**
+     *
+     * @param countryStats
+     */
+    initMap = (countryStats) =>{
+        countryStats.forEach(country =>{
+            this.renderCountryInMap(country.country,country.country_total_stats);
+        });
+    };
 
     render() {
-        return <WorldMapSvg />;
+        this.initMap(this.props.country_stats);
+        Object.entries(document.querySelectorAll('#svgMapWrapper>svg>path')).map((object) => {
+            object[1].addEventListener("mouseover",function(){
+                document.getElementById("tooltip").classList.add("active");
+                document.getElementById("tooltip").innerHTML = this.getAttribute("datainfo");
+            });
+            object[1].addEventListener("mouseout",function(){
+                document.getElementById("tooltip").classList.remove("active");
+                document.getElementById("tooltip").style.top = "";
+                document.getElementById("tooltip").style.left = "";
+                document.getElementById("tooltip").innerHTML = "";
+            });
+            object[1].addEventListener("mousemove",function(e){
+                document.getElementById("tooltip").setAttribute("style","top:"+e.pageY+"px; left:"+e.pageX+"px;");
+            });
+            object[1].addEventListener("click",function(){
+                //get chart data
+                //update chart -> another component
+            });
+        });
+
+        return (
+            <div id="world_map_container">
+                <div id="tooltip"></div>
+                <div id="svgMapWrapper">
+                    <WorldMapSvg />
+                </div>
+            </div>
+            );
     }
 }
 
--- src/pages/Stats/Stats.js
+++ src/pages/Stats/Stats.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from "react";
+import axios from 'axios';
 import Loader from 'react-loader-spinner';
 import WorldMap from "../../components/WorldMap/WorldMap";
 
@@ -10,29 +11,27 @@ class Stats extends Component{
     state = {
         loadingData: true,
         totalWorldStats : {},
-        countryStats : {},
+        countryStats : [],
     };
 
     componentDidMount() {
-        fetch(apiUrl+'/coronawatch/stats')
-            .then(res => {
-                if(res.status !== 200){
-                    throw new Error('Failed to get data');
-                }
-                return res.json();
-            })
-            .then(resData => {
-                this.setState({loadingData: false});
-                this.setState({totalWorldStats: resData.total_world_stats});
-                this.setState({countryStats: resData.country_stats});
-                //console.log(resData);
-            })
-            .catch(err => {
-                //dialog box cant get fresh data
-                //get from local storage
-                throw new Error('Failed to get data');
-            });
+        try{
+            this.getData().catch(err => {throw new Error('Failed to get data');});
+        }catch (e) {
+            //dialog box cant get fresh data
+            //get from local storage
+            throw new Error('Failed to get data');
+        }
     }
+
+    getData = async () => {
+        const response = await axios(apiUrl+'/coronawatch/stats');
+        const data = response.data;
+        this.setState({loadingData: false});
+        this.setState({totalWorldStats: data.world_stats[0]});
+        this.setState({countryStats: data.country_stats});
+    };
+
     render() {
         return (
             <Fragment>
@@ -40,7 +39,6 @@ class Stats extends Component{
                     <Loader type="ThreeDots" color="#2BAD60" height={100} width={100} />
                 ) : <WorldMap
                     country_stats = {this.state.countryStats}
-                    testVal = "hello"
                 /> }
             </Fragment>
         )
