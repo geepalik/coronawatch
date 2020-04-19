295b770ab2f31f3436d11738bef553007011766e
gilp <gil.palikaras@gmail.com>
2020-04-19 20:47:57 +0300

    when selecting multiple countries show charts with their stats in popup

--- src/components/Charts/CountriesStatsPopup/CountriesStatsPopup.js
+++ src/components/Charts/CountriesStatsPopup/CountriesStatsPopup.js
@@ -3,35 +3,124 @@ import {Line} from "react-chartjs-2";
 import Modal from "../../Modal/Modal";
 import Backdrop from "../../Backdrop/Backdrop";
 
+import './CountriesStatsPopup.css';
+
 class CountriesStatsPopup extends Component{
 
     constructor(props) {
         super(props);
         this.state = {
-            chartData : {datasets: []}
+            singleChartData : {datasets: []},
+            multipleChartsData : {
+                'Confirmed' : {
+                    labels : '',
+                    datasets: []
+                },
+                'Deaths' : {
+                    labels : '',
+                    datasets: []
+                },
+                'Recovered' : {
+                    labels : '',
+                    datasets: []
+                }
+            }
         }
     }
 
     /**
-     * create chart for clicked country
-     * TODO prepare case when comparing countries
+     * create charts for single clicked country and country compare
      * @param prevProps
      * @param prevState
      * @param snapshot
      */
     componentDidUpdate(prevProps, prevState, snapshot) {
-        //for now, one country
-        const countryName = this.props.countriesSelected[0];
-        if(countryName && prevProps.countriesSelected[0] !== countryName){
-            this.prepareChart(countryName);
+        if(
+            this.props.modalOpen &&
+            this.props.countriesSelected.length > 0
+        ){
+            if(
+                this.props.countriesSelected.length === 1 &&
+                prevProps.countriesSelected !== this.props.countriesSelected
+            ){
+                const countryName = this.props.countriesSelected[0];
+                this.setState({singleChartData: this.prepareSingleChart(countryName)})
+            }else if(
+                this.props.countriesSelected.length > 1 &&
+                this.state.multipleChartsData.Confirmed.datasets.length === 0
+            ){
+                this.setState({multipleChartsData: this.prepareMultipleCharts(this.props.countriesSelected)})
+            }
         }
+    }
+
+    /**
+     * 16777215 == ffffff in decimal
+     * @returns {string}
+     */
+    getRandomColor = () =>{
+        return '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    /**
+     * render charts for multiple selected countries (compare mode)
+     * @param selectedCountries
+     * @returns {{Recovered: {datasets: [], labels: string}, Deaths: {datasets: [], labels: string}, Confirmed: {datasets: [], labels: string}}}
+     */
+    prepareMultipleCharts = (selectedCountries) =>{
+        const multiCharts = {
+            'Confirmed' : {
+                labels : '',
+                datasets: []
+            },
+            'Deaths' : {
+                labels : '',
+                datasets: []
+            },
+            'Recovered' : {
+                labels : '',
+                datasets: []
+            }
+        };
+
+        selectedCountries.forEach(countryName => {
+            const countryData = this.props.getRowFromObject(this.props.countryStats,'country',countryName);
+            const {xTime, yConfirmed, yDeaths, yRecovered} = this.props.getChartData(countryData[0].stats);
+
+            multiCharts.Confirmed.labels = xTime;
+            multiCharts.Confirmed.datasets.push(
+                {
+                    label: countryName+' Confirmed Cases',
+                    borderColor: this.getRandomColor(),
+                    data: yConfirmed,
+                    fill: false
+                }
+            );
+
+            multiCharts.Deaths.labels = xTime;
+            multiCharts.Deaths.datasets.push({
+                label: countryName+' Death Cases',
+                borderColor: this.getRandomColor(),
+                data: yDeaths,
+                fill: false
+            });
+
+            multiCharts.Recovered.labels = xTime;
+            multiCharts.Recovered.datasets.push({
+                label: countryName+' Recovered Cases',
+                borderColor: this.getRandomColor(),
+                data: yRecovered,
+                fill: false
+            });
+        });
+        return multiCharts;
     }
 
     /**
      * render chart
      * @param countryName
      */
-    prepareChart = (countryName) =>{
+    prepareSingleChart = (countryName) =>{
         const countryData = this.props.getRowFromObject(this.props.countryStats,'country',countryName);
         const {xTime, yConfirmed, yDeaths, yRecovered} = this.props.getChartData(countryData[0].stats);
         const newChartData = {datasets: []};
@@ -54,7 +143,7 @@ class CountriesStatsPopup extends Component{
             data: yRecovered,
             fill: false
         });
-        this.setState({chartData: newChartData})
+        return newChartData;
     };
 
     /**
@@ -62,21 +151,59 @@ class CountriesStatsPopup extends Component{
      */
     closeModal = () => {
         this.props.onCloseModal();
-        this.setState({chartData: {datasets: []}})
+        this.setState({
+            chartData: {datasets: []},
+            multipleChartsData : {
+                'Confirmed' : {
+                    labels : '',
+                    datasets: []
+                },
+                'Deaths' : {
+                    labels : '',
+                    datasets: []
+                },
+                'Recovered' : {
+                    labels : '',
+                    datasets: []
+                }
+            }
+        })
     };
 
     render() {
-        return (this.props.countriesSelected.length >= 1 && this.props.modalOpen) ?(
+        return (this.props.modalOpen) ?(
             <Fragment>
                 <Backdrop onClick={this.closeModal}/>
                 <Modal
                     onCloseModal={this.closeModal}
                 >
-                    {this.state.chartData.datasets.length > 0 ? (
+                    {this.props.countriesSelected.length === 1 ? (
                         <Line
-                            data={this.state.chartData}
+                            data={this.state.singleChartData}
                             options={this.props.chartOptions}
                         />
+                    ) : null}
+                    {this.props.countriesSelected.length > 1 ? (
+                        <div className="multi-charts-wrapper">
+                            <div className="chart-container">
+                                <Line
+                                    data={this.state.multipleChartsData.Confirmed}
+                                    options={this.props.chartOptions}
+                                />
+                            </div>
+                            <div className="chart-container">
+                                <Line
+                                    data={this.state.multipleChartsData.Deaths}
+                                    options={this.props.chartOptions}
+                                />
+                            </div>
+                            <div className="chart-container">
+                                <Line
+                                    data={this.state.multipleChartsData.Recovered}
+                                    options={this.props.chartOptions}
+                                />
+                            </div>
+                        </div>
                     ) : null}
                 </Modal>
             </Fragment>
